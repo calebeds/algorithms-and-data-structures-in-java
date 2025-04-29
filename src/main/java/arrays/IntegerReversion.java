8fa439da35b95e507b6a8f18c23a30fdc4700cce
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-04-29 20:27:54 -0300

    add integer reversion solution

---
+++ src/main/java/arrays/IntegerReversion.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class IntegerReversion {
+    public int reverse(int n) {
+        int reversed = 0;
+        int remainder = 0;
+
+        while(n > 0) {
+            remainder = n % 10;
+            n = n / 10;
+            reversed = reversed * 10 + remainder;
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        IntegerReversion problem = new IntegerReversion();
+        System.out.println(problem.reverse(454515223));
+    }
+}
