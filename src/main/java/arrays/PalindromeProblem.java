37d8769b01c380a3280b97ef6043a2d540bdc13f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-04-29 20:16:39 -0300

    add palindrome problem solution

---
+++ src/main/java/arrays/PalindromeProblem.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class PalindromeProblem {
+    // it has O(n) linear running time complexity
+    public boolean solve(String s) {
+        int forward = 0;
+        int backward = s.length() - 1;
+
+        while (forward < backward) {
+            if(s.charAt(forward) != s.charAt(backward)) {
+                return false;
+            }
+            forward++;
+            backward--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        PalindromeProblem problem = new PalindromeProblem();
+        System.out.println(problem.solve("madam"));
+    }
+}
