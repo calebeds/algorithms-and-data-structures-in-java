161f5bcfd3f889b15d578dc7ccededb9dc0559a3
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-09 16:31:54 -0300

    add splash tree example

---
+++ src/main/java/splaytree/SplayTreeTest.java
@@ -0,0 +1,22 @@
+package splaytree;
+
+public class SplayTreeTest {
+    public static void main(String[] args) {
+        Tree<Integer> splayTree = new SplayTree<>();
+
+        splayTree.insert(1);
+        System.out.println(splayTree.getRoot());
+
+        splayTree.insert(10);
+        System.out.println(splayTree.getRoot());
+
+        splayTree.insert(4);
+        System.out.println(splayTree.getRoot());
+
+        splayTree.insert(5);
+        System.out.println(splayTree.getRoot());
+
+        splayTree.insert(-2);
+        System.out.println(splayTree.getRoot());
+    }
+}
