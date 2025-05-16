c7cc43daf3407d26350a6e1ba98045d5b3d88c7a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-16 20:20:03 -0300

    check min heap example

---
+++ src/main/java/heaps/checkminheap/CheckMinHeapExample.java
@@ -0,0 +1,12 @@
+package heaps.checkminheap;
+
+public class CheckMinHeapExample {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5};
+
+        CheckHeap checkHeap = new CheckHeap();
+
+        System.out.println(checkHeap.isMinHeap(nums));
+    }
+}
