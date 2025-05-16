a996531921e982934bf4b259c77cb0efe4a1c38a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-16 20:20:57 -0300

    fix small mistake on check heap

--- src/main/java/heaps/checkminheap/CheckHeap.java
+++ src/main/java/heaps/checkminheap/CheckHeap.java
@@ -5,7 +5,7 @@ public class CheckHeap {
         // leaf nodes do not have children
         // if 2 * i + 2 >= N then we know that node is a leaf node (no need to consider)
         // so we have to rearrange the equation to get how many nodes to consider
-        for (int i = 0; i < (heap.length - 2) / 2; i++) {
+        for (int i = 0; i <= (heap.length - 2) / 2; i++) {
             if (heap[1] > heap[2 * i + 1] || heap[i] > heap[2 * i + 2]) {
                 return false;
             }
