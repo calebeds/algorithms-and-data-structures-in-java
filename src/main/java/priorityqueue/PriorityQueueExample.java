46021ad613fc8ff7127f7ba7f3154bd5bb56342e
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-16 16:59:22 -0300

    add priority queue in java example

---
+++ src/main/java/priorityqueue/PriorityQueueExample.java
@@ -0,0 +1,45 @@
+package priorityqueue;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        // by default the heap is MIN HEAP
+        // MAX HEAP when provide Collections.reverseOrder()
+//        Queue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+//
+//        // O(logN)
+//        heap.add(12);
+//        heap.add(5);
+//        heap.add(7);
+//        heap.add(0);
+//        heap.add(-2);
+//
+//        // O(N)
+//        heap.contains(0);
+//
+//        // when we consider all N items in O(logN) = O(NlogN)
+//        while (!heap.isEmpty()) {
+//            // O(logN)
+//            System.out.println(heap.poll());
+//        }
+
+        Queue<Person> heap = new PriorityQueue<>();
+
+        // O(logN)
+        heap.add(new Person("Kevin", 34));
+        heap.add(new Person("Daniel", 12));
+        heap.add(new Person("Ana", 67));
+        heap.add(new Person("Adam", 18));
+        heap.add(new Person("Michael", 33));
+
+
+        // when we consider all N items in O(logN) = O(NlogN)
+        while (!heap.isEmpty()) {
+            // O(logN)
+            System.out.println(heap.poll());
+        }
+    }
+}
