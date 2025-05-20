15927f5cfb0b1b78c82de7e62e2006940d84c7d2
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-20 13:17:55 -0300

    add hash code and equals example on hash maps

---
+++ src/main/java/hashtable/hashcodeandequals/HashCodeEqualsExample.java
@@ -0,0 +1,10 @@
+package hashtable.hashcodeandequals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashCodeEqualsExample {
+    public static void main(String[] args) {
+        Map<Person, String> person = new HashMap<>();
+    }
+}
