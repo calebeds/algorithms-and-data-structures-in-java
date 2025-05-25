29da0cd48e951cc51a9b4803cecf7bb682a429d3
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-25 09:58:06 -0300

    add brute force search

---
+++ src/main/java/search/bruteforcesearch/BruteForceSearchTest.java
@@ -0,0 +1,8 @@
+package search.bruteforcesearch;
+
+public class BruteForceSearchTest {
+    public static void main(String[] args) {
+        BruteForceSearch bruteForceSearch = new BruteForceSearch();
+        System.out.println(bruteForceSearch.search("ttttta", "tta"));
+    }
+}
