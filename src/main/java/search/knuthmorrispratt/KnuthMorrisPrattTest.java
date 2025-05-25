b579bd2876d5bd95ca71720c0bdad55324b72777
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-25 14:01:56 -0300

    add knuth morris pratt algorithm implementation

---
+++ src/main/java/search/knuthmorrispratt/KnuthMorrisPrattTest.java
@@ -0,0 +1,9 @@
+package search.knuthmorrispratt;
+
+public class KnuthMorrisPrattTest {
+    public static void main(String[] args) {
+        KnuthMorrisPratt knuthMorrisPratt = new KnuthMorrisPratt("This is test!", "test");
+
+        knuthMorrisPratt.search();
+    }
+}
