04c066ab8998c287bb327fda51ec1e1bdafc09fa
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-05-26 20:09:50 -0300

    add z algorithm example

---
+++ src/main/java/search/zalgorithm/ZAlgorithmTest.java
@@ -0,0 +1,8 @@
+package search.zalgorithm;
+
+public class ZAlgorithmTest {
+    public static void main(String[] args) {
+        ZAlgorithm zAlgorithm = new ZAlgorithm("This Test is a Test!", "Test");
+        zAlgorithm.search();
+    }
+}
